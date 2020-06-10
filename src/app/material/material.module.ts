8251c53fdf9a2b33e7f36ff1568dfa3b357137bc
Michael Orecchia <morecchia@gravitystack.com>
2020-06-10 16:39:49 -0700

    some cleanup; change input colors/font-size

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,19 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatSliderModule} from '@angular/material/slider';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatTooltipModule } from '@angular/material/tooltip';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 const MATERIAL = [
-  MatSidenavModule,
   MatListModule,
   MatSliderModule,
   MatButtonModule,
