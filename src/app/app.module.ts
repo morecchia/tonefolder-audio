c3493e7f9242ace6799e2173758e18047dc1b3af
morecchia <michael.orecchia@gmail.com>
2022-03-25 14:52:02 -0700

    remove unnecessary material import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthModule, AuthHttpInterceptor } from '@auth0/auth0-angular';
 import { authConfig } from '../configurations/auth.configuration';
-import { MaterialModule } from './shared/material/material.module';
 import { CoreModule } from './core/core.module';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -17,7 +16,6 @@ import { AppRoutingModule } from './app-routing.module';
     BrowserModule,
     HttpClientModule,
     AuthModule.forRoot(authConfig),
-    MaterialModule,
     BrowserAnimationsModule,
     CoreModule,
     AppRoutingModule,
